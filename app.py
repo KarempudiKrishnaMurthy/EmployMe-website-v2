676771cfed80505593e93c87e44e4251a6af8882
KarempudiKrishnaMurthy <112862797+KarempudiKrishnaMurthy@users.noreply.github.com>
2023-04-23 19:06:05 +0000

    added dynamic buttons

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [
+  {
+    'id': 1,
+    'title': "Data Analyst",
+    'location': 'Bangaluru,India',
+    'salary': 'Rs.10,00,000'
+  },
+  {
+    'id': 2,
+    'title': "Data Scientist",
+    'location': 'Delhi,India',
+    'salary': 'Rs.15,00,000'
+  },
+  {
+    'id': 3,
+    'title': "Frontend Engineer",
+    'location': 'Remote',
+    'salary': 'Rs.12,00,000'
+  },
+  {
+    'id': 4,
+    'title': "Data Scientist",
+    'location': 'San Francisco,USA',
+    'salary': '$120,000'
+  },
+]
 
 
 @app.route("/")
 def hello_world():
-  return 'Hello,World ram'
+  return render_template('home.html', jobs=JOBS, company_name="EmployMe")
+
+
+@app.route("/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
