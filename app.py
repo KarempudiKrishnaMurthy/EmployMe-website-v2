11bab7b726cced20c0936fb74f316607cc0f18d4
KarempudiKrishnaMurthy <112862797+KarempudiKrishnaMurthy@users.noreply.github.com>
2023-04-25 17:42:58 +0000

    Application form

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db,load_job_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
@@ -15,10 +15,12 @@ def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
 
+
 # @app.route("/job/<id>")
 # def show_job(id):
 #   job=load_job_from_db(id)
 #   return render_template('jobpage.html',job=job)
+
 
 @app.route("/job/<id>")
 def show_job(id):
@@ -28,6 +30,13 @@ def show_job(id):
     return "Not Found", 404
 
   return render_template('jobpage.html', job=job)
+
+
+@app.route("/job/<id>/apply", methods=["post"])
+def apply_to_job(id):
+  data = request.form
+  return render_template('application_submited.html', application=data)
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
